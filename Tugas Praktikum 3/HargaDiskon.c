2162826774a99d87494e0f75598a2c3fe9ecd1d2
hafizattariq <99478882+hafizattariq@users.noreply.github.com>
2022-03-17 14:28:43 +0700

    Update HargaDiskon.c

--- Tugas Praktikum 3/HargaDiskon.c
+++ Tugas Praktikum 3/HargaDiskon.c
@@ -18,26 +18,26 @@ int main(){
 	scanf(" %c", &p);
 
     switch(p){
-    case 'A' :
-			d = 0.1;
-			s = i - (i * d);
-			printf("Harga setelah diskon %.2f", s);
-			break;
+    	case 'A' :
+		d = 0.1;
+		s = i - (i * d);
+		printf("Harga setelah diskon %.2f", s);
+		break;
 
-		case 'B' :
-			d = 0.15;
-			s = i - (i * d);
-			printf("Harga setelah diskon %.2f", s);
-			break;
+	case 'B' :
+		d = 0.15;
+		s = i - (i * d);
+		printf("Harga setelah diskon %.2f", s);
+		break;
 
-		case 'C' :
-			d = 0.2;
-			s = i - (i * s);
-			printf("Harga setelah diskon %.2f", s);
-			break;
+	case 'C' :
+		d = 0.2;
+		s = i - (i * s);
+		printf("Harga setelah diskon %.2f", s);
+		break;
 
-		default :
-			printf("Bukan merupakan pilihan diskon");
+	default :
+		printf("Bukan merupakan pilihan diskon");
 	}
 	return 0;
 }
